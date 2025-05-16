19205a2c0e36402cea3a3d6191c0e247d3f88578
JamunaSundaramSF3699 <jamuna.sundaram@syncfusion.com>
2025-05-16 17:59:40 +0530

    Updated the sample

---
+++ add_range_items_observableCollection/ObservableCollection/Components/Data/OrdersDetailsObserveData.cs
@@ -0,0 +1,21 @@
+﻿namespace ObservableCollection.Components.Data
+{
+    public class OrdersDetailsObserveData
+    {
+        public int? OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public double? Freight { get; set; }
+
+        public static IEnumerable<OrdersDetailsObserveData> GetAllRecords()
+        {
+            return Enumerable.Range(1, 10).Select(x => new OrdersDetailsObserveData
+            {
+                OrderID = 1000 + x,
+                CustomerID = (new[] { "ALFKI", "ANANTR", "ANTON", "BLONP", "BOLID" })[new Random().Next(5)],
+                Freight = Math.Round(2.1 * x, 2),
+                OrderDate = DateTime.Now.AddDays(-x)
+            }).ToList();
+        }
+    }
+}
